180e5f654be46a36cc242e6db8d887646de17e4c
almsallaty <a.h.almsallaty@gmail.com>
2026-07-15 14:37:02 +0200

    Add files via upload

---
+++ sale_daily_product_report/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Professional Product Sales Report",
+    "summary": "Bilingual daily, weekly, monthly, and custom product sales PDF report",
+    "version": "19.0.1.0.3",
+    "category": "Sales/Reporting",
+    "author": "Custom",
+    "license": "LGPL-3",
+    "depends": ["sale_management"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_sales_report_wizard_views.xml",
+        "report/product_sales_report_action.xml",
+        "report/product_sales_report_templates.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
